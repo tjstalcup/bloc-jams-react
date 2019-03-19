623e0680c5e856c06cf9ca738fc9fc272f0bb544
Meredith Harris <meredithharris@SDavidWesiPhone.hookedoncolfax.guest>
2019-03-19 16:56:57 -0600

    finished album checkpoint

--- src/components/Album.js
+++ src/components/Album.js
@@ -33,12 +33,14 @@ class Album extends Component {
                         <col group="song-duration-column" />
                     </colgroup>
                     <tbody>
-                        <div className="album-songs">{this.state.album.songs.map((songs, index) => (
-                                <tr key= {`song-${index}`}>
-                                    {songs}
-                                </tr>
-                            ))}
-                        </div>
+                        {this.state.album.songs.map((song, index) =>
+                            <tr className="song" key={index}>
+                                <td className="song-number">{index+1}</td>
+                                <td className="song-title">{song.title}
+                                </td>
+                                <td className="song-duration">{song.duration}</td>
+                            </tr>
+                        )}
                     </tbody>
                 </table>
             </section>
